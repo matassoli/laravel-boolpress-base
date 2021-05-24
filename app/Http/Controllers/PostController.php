40a304ddfdcec72ac580037543ac546d075d431d
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-05-24 21:26:30 +0200

    controller test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Post;
 
 use Illuminate\Http\Request;
 
@@ -13,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+      $posts = Post::all();
+      dd($posts);
     }
 
     /**
@@ -43,9 +45,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+      dd($post->comments);
     }
 
     /**
