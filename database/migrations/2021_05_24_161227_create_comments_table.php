f3df3f279f033ce68d1126cf759cb48dc2c4fcc6
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-05-24 18:33:29 +0200

    model

--- database/migrations/2021_05_24_161227_create_comments_table.php
+++ database/migrations/2021_05_24_161227_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->date('date');
+            $table->foreignId('post_id')->constrained();
+            $table->string('name', 100)->nullable();
             $table->text('content');
-            $table->string('image')->nullable();
-            $table->string('slug')->unique();
-            $table->boolean('published')->default(0);
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 }
